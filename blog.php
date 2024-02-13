31b0929e91acc3285b23fee4c763e765716e291e
nprdservice1 <nprdservice1@gmail.com>
2024-02-13 16:08:00 +0545

    initial commit - register

---
+++ blog.php
@@ -0,0 +1,268 @@
+<!doctype html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>NCLEX Nepal | Empowering Healthcare Profession for Better Future </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/icomoon.css">
+    <link rel="stylesheet" href="/css/owl.carousel.min.css">
+    <link rel="icon" type="image/x-icon" href="/images/logo-n.png">
+    <meta name="title" content="Opportunities for Nepalese Nurses in USA, UK, and Australia">
+    <meta name="description" content=" The doors to international opportunities are wide open, especially in the USA, UK, and Australia, presenting you with a chance to make a mark on the global healthcare stage. Let's delve into the exciting prospects awaiting you in these three distinct destinations.">
+
+    <meta property="og:image" content="https://register.nclexinnepal.com/images/banner/596797ea-f3bf-4b36-bb97-df5d697bceb7.jpeg">
+    <meta name="twitter:image" content="https://register.nclexinnepal.com/images/banner/596797ea-f3bf-4b36-bb97-df5d697bceb7.jpeg">
+    <meta name="twitter:title" content="Opportunities for Nepalese Nurses in USA, UK, and Australia">
+
+
+    <link rel="stylesheet" href="/css/customScrollbar.css">
+    <link rel="stylesheet" href="/css/photoswipe.css">
+    <link rel="stylesheet" href="/css/default-skin.css">
+    <link rel="stylesheet" href="/css/prettyPhoto.css">
+    <link rel="stylesheet" href="/css/animate.css">
+    <link rel="stylesheet" href="/css/transitions.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/color.css">
+    <link rel="stylesheet" href="/css/responsive1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1//css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script src="/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <style>
+        p{
+            font-size: 16px; line-height: 28px;
+            font-family: 'Montserrat', Arial, Helvetica, sans-serif;
+        }
+        .social{
+            font-size: 25px!important;
+        }
+    </style>
+</head>
+<body id="body" class="tg-home tg-hometwo">
+<!--[if lt IE 8]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
+    your browser</a> to improve your experience.</p>
+<![endif]-->
+<!--************************************
+        Wrapper Start
+*************************************-->
+<div id="tg-wrapper" class="tg-wrapper tg-haslayout">
+
+    <header id="tg-header" class="tg-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <strong class="tg-logo"><a href="/"><img src="/images/logo.png" alt="company logo here"></a></strong>
+                    <div class="tg-navigationarea">
+                        <!--							<a class="tg-btn tg-btnbecommember" href="javascript:void(0);">Book Your Seat</a>-->
+                        <nav id="tg-nav" class="tg-nav">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                        data-target="#tg-navigation" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+                            <div id="tg-navigation" class="collapse navbar-collapse tg-navigation">
+                                <div class="header-email">
+                                    Contact:
+                                    <a href="tel:+977 01 4168241"> 01 4168241 </a> <span
+                                        style="margin: 0 10px;">|</span>
+                                    <a href="tel:+977 01 4168103"> 01 4168103</a>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</div>
+<!--************************************
+        Home Slider End
+*************************************-->
+<!--************************************
+        Main Start
+*************************************-->
+<main id="tg-main" class="tg-main tg-haslayout">
+    <!--************************************
+            About Us Start
+    *************************************-->
+    <section class="tg-haslayout tg-sectionspace tg-bgaddtocalendar" style="background-image: none">
+        <div class="container">
+            <div class="tg-eventinfo tg-eventinfovtwo">
+                <div class="tg-eventcontent">
+                    <!--                    <span>NCLEX | OET | OBA</span>-->
+                    <h2 style="margin: 0">Opportunities for Nepalese Nurses in USA, UK, and Australia</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="tg-haslayout tg-sectionspace tg-bglight">
+
+        <div class="container">
+            <!--            <h1>Opportunities for Nepalese Nurses in USA, UK, and Australia</h1>-->
+
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 " >
+                    <div style="display: flex; align-items: center; grid-gap: 6px; margin-bottom: 5px">
+
+                        <i class="fa fa-share-alt " style="color: gray;"></i>
+                        <a target="_blank" href="https://www.facebook.com/sharer?u=register.nclexinnepal.com/blog&amp;p[images][0]=https://register.nclexinnepal.com/images/banner/596797ea-f3bf-4b36-bb97-df5d697bceb7.jpeg">
+                            <i class="fa-brands fa-square-facebook social" style="color: #4267B2;"></i>
+                        </a>
+                        <a target="_blank" href="https://twitter.com/intent/tweet?text=Opportunities for Nepalese Nurses in USA, UK, and Australia. For more detail: &url=register.nclexinnepal.com/blog">
+
+                            <i class="fa-brands fa-square-x-twitter social" style="color: black"></i>
+                        </a>
+<!--                        <i class="fa fa- social" style="color: #1DA1F2;"></i>-->
+                    </div>
+
+                    <!--                    <img src="https://register.nclexinnepal.com/images/39be42f7-cb56-4ce2-b765-86cbd9f0af02.jpeg" style="margin: 0 0 25px 25px; width: 550px; float: right">-->
+                    <img src="/images/banner/blog.jpeg" style="margin: 0 0 25px 25px; width: 700px; float: right">
+
+                    <p>Nepalese nurses, are you ready to take your nursing career to unprecedented heights? The doors to international opportunities are wide open, especially in the USA, UK, and Australia, presenting you with a chance to make a mark on the global healthcare stage. Let's delve into the exciting prospects awaiting you in these three distinct destinations.
+                    </p>
+                    <p>
+
+                        <strong>1. United States</strong> - A Land of Possibilities:
+                        The USA, renowned for its cutting-edge healthcare system, beckons Nepalese nurses with promising opportunities. Nclex Nepal understands the unique challenges posed by the Nclex exam, a crucial step for nurses aspiring to practice in the United States. Our comprehensive support ensures you are well-prepared to navigate the exam and seamlessly transition into the American healthcare landscape. The Nursing Information Tour 2024 will shed light on the intricacies of nursing in the USA, providing invaluable insights for those ready to embrace this career-defining move.
+                    </p>
+                    <p>
+
+                        <strong>2. United Kingdom</strong> - Pioneering Healthcare Excellence:
+                        The UK, a hub for pioneering healthcare practices, opens its doors to Nepalese nurses seeking to contribute to its esteemed National Health Service (NHS). Nclex Nepal's expertise in preparing nurses for the Occupational English Test (OET) and Computer-Based Test (CBT) ensures a smooth pathway to pre-registration in the UK. The Nursing Information Tour 2024 will feature in-depth sessions on the nuances of working in the UK, offering a glimpse into the rich tapestry of opportunities available in this healthcare haven.
+                    </p>
+
+                    <p>
+
+                        <strong>3. Australia</strong> - Embracing Diversity in Healthcare:
+                        Australia, known for its multicultural society and high-quality healthcare, invites Nepalese nurses to become part of its dynamic healthcare workforce. Nclex Nepal's support in preparing for the Nclex exam tailored for Australia ensures that you are ready to meet the country's stringent standards. The Nursing Information Tour 2024 will explore the diverse opportunities available down under, providing a roadmap for nurses keen on contributing to Australia's healthcare excellence.
+                    </p>
+                    <p>
+
+                        Don't miss the chance to shape your nursing future—register now for the Nursing Information Tour 2024 at <a href="https://register.nclexinnepal.com"> https://register.nclexinnepal.com/</a>. Join us on this journey of discovery, where opportunities abound, and the global stage awaits your nursing expertise.
+                    </p>
+                    <p>
+                        #NursingTour2024 #NclexNepal #NurseOpportunities
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!--************************************
+            News & Articles End
+    *************************************-->
+</main>
+<!--************************************
+        Main End
+*************************************-->
+<!--************************************
+        Footer Start
+*************************************-->
+<footer id="tg-footer" class="tg-footer tg-haslayout border ">
+    <!--************************************
+            Contact Us Start
+    *************************************-->
+    <!--			<div class="tg-contastus tg-bgcontactus tg-sectionspace">-->
+    <!--				<h1>Want to ask something?</h1>-->
+    <!--				<div class="container">-->
+    <!--					<div class="row">-->
+    <!--						<div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-2 col-lg-8">-->
+    <!--							<div class="tg-sectionhead">-->
+    <!--								<div class="tg-sectionheading">-->
+    <!--									<span>Don’t worry we love to help</span>-->
+    <!--									<h2>Get in touch with us</h2>-->
+    <!--								</div>-->
+    <!--							</div>-->
+    <!--						</div>-->
+    <!--						<div class="col-xs-12 col-xs-offset-0 col-sm-12 col-sm-offset-0 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">-->
+    <!--							<form class="tg-formtheme tg-formcontactus">-->
+    <!--								<fieldset>-->
+    <!--									<div class="form-group">-->
+    <!--										<input type="text" name="fullname" class="form-control" placeholder="Full Name*">-->
+    <!--									</div>-->
+    <!--									<div class="form-group">-->
+    <!--										<input type="email" name="email" class="form-control" placeholder="Email*">-->
+    <!--									</div>-->
+    <!--									<div class="form-group">-->
+    <!--										<input type="text" name="phone" class="form-control" placeholder="Phone*">-->
+    <!--									</div>-->
+    <!--									<div class="form-group">-->
+    <!--										<input type="text" name="subject" class="form-control" placeholder="Subject*">-->
+    <!--									</div>-->
+    <!--									<div class="form-group">-->
+    <!--										<textarea placeholder="Message*"></textarea>-->
+    <!--									</div>-->
+    <!--									<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">-->
+    <!--										<button class="tg-btn" type="submit">Send Now</button>-->
+    <!--									</div>-->
+    <!--								</fieldset>-->
+    <!--							</form>-->
+    <!--						</div>-->
+    <!--					</div>-->
+    <!--				</div>-->
+    <!--			</div>-->
+    <!--************************************
+            Contact Us End
+    *************************************-->
+    <!--************************************
+            Bottom Bar Start
+    *************************************-->
+    <div class="tg-footerbar tg-bglight">
+        <a id="tg-btnscrolltop" class="tg-btnscrolltop" href="javascript:void(0);"><i
+                class="icon-chevron-up"></i></a>
+        <div class="container">
+            <div class="row">
+                <ul class="tg-socialicons pull-right">
+                    <li class="tg-facebook"><a href="https://www.facebook.com/nclexnepal.official/"><i class="fa-brands fa-facebook-f"></i></a></li>
+                    <li class="tg-instagram"><a href="https://www.instagram.com/nclexnepal.official/"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li class="tg-linkedin"><a href="https://www.linkedin.com/company/nclex-nepal/"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                </ul>
+                <p class="tg-copyrights pull-left">2024 All Rights Reserved © Nclex Nepal</p>
+            </div>
+        </div>
+    </div>
+    <!--************************************
+            Bottom Bar End
+    *************************************-->
+</footer>
+<!--************************************
+        Footer End
+*************************************-->
+</div>
+<!--************************************
+        Wrapper End
+*************************************-->
+<script src="/js/vendor/jquery-library.js"></script>
+<script src="/js/vendor/bootstrap.min.js"></script>
+<script src="https://maps.google.com/maps/api/js?key=AIzaSyCR-KEWAVCn52mSdeVeTqZjtqbmVJyfSus&language=en"></script>
+<script src="/js/jquery.singlePageNav.min.js"></script>
+<script src="/js/photoswipe-ui-default.js"></script>
+<script src="/js/customScrollbar.min.js"></script>
+<script src="/js/owl.carousel.min.js"></script>
+<script src="/js/photoswipe.min.js"></script>
+<script src="/js/prettyPhoto.js"></script>
+<script src="/js/tilt.jquery.js"></script>
+<script src="/js/countdown.js"></script>
+<script src="/js/parallax.js"></script>
+<script src="/js/gmap3.js"></script>
+<script src="/js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,97 @@
+"use strict";
+jQuery(document).on('ready', function() {
+	/* -------------------------------------
+	 PRELOADER
+	 -------------------------------------- */
+	jQuery("#status").delay(2000).fadeOut();
+	jQuery("#preloader").delay(2000).fadeOut("slow");
+	/* ---------------------------------------
+			STICKY HEADER
+	--------------------------------------- */
+	if(jQuery('#tg-header').length > 0){
+		jQuery(window).on('scroll', function(){
+			if(jQuery(this).scrollTop() > 59){
+				jQuery('body').addClass('tg-fixedme');
+			} else {
+				jQuery('body').removeClass('tg-fixedme');
+			}
+		});
+	}
+	/* -------------------------------------
+			SCROLL NAV
+	-------------------------------------- */
+	var body = jQuery('body');
+	if(body.hasClass('tg-home')){
+		body.addClass("home");
+		jQuery(window).on('scroll', function() {
+			var scroll = jQuery(window).scrollTop();
+			if (scroll >= 10) {
+				jQuery("#tg-header").addClass("single-page-nav");
+			}else {
+				jQuery("#tg-header").removeClass("single-page-nav");
+			}
+		});
+	}
+	// var _tg_navigation = jQuery('.tg-navigation');
+	// _tg_navigation.singlePageNav({
+	// 	updateHash: false,
+	// 	offset: 90,
+	// 	filter: ':not(.external)',
+	// });
+	var _tg_btnscrolltop = jQuery("#tg-btnscrolltop");
+	_tg_btnscrolltop.on('click', function(){
+		var _scrollUp = jQuery('html, body');
+		_scrollUp.animate({ scrollTop: 0 }, 'slow');
+	})
+
+	/* -------------------------------------
+			NEXT EVENT COUNTER
+	-------------------------------------- */
+	var _tg_upcomingeventcounter = jQuery('#tg-upcomingeventcounter');
+	_tg_upcomingeventcounter.countdown('2024/02/07', function(event) {
+		var $this = jQuery(this).html(event.strftime(''
+			+ '<div class="tg-eventcounter"><span>%-D</span><span> Days Left</span></div>'
+			+ '<div class="tg-eventcounter"><span>%H</span><span>Hours</span></div>'
+			+ '<div class="tg-eventcounter"><span>%M</span><span>Minutes</span></div>'
+			+ '<div class="tg-eventcounter"><span>%S</span><span>Seconds</span></div>'
+		));
+	});
+	/* -------------------------------------
+			NEXT EVENT COUNTER
+	-------------------------------------- */
+	var _tg_upcomingeventcountervtwo = jQuery('#tg-upcomingeventcountervtwo');
+	_tg_upcomingeventcountervtwo.countdown('2024/02/07', function(event) {
+		var $this = jQuery(this).html(event.strftime(''
+			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%-D</span><span> Days</span></div></div>'
+			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%H</span><span>Hours</span></div></div>'
+			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%M</span><span>Minutes</span></div></div>'
+			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%S</span><span>Seconds</span></div></div>'
+		));
+	});
+	/* -------------------------------------
+			STICKY HEADER
+	-------------------------------------- */
+	jQuery(window).on('scroll', function() {
+		var scroll = jQuery(window).scrollTop();
+		if (scroll >= 900) {
+			jQuery('.tg-header.tg-headervthree').addClass('tg-showheader');
+		} else {
+			jQuery('.tg-header.tg-headervthree').removeClass('tg-showheader');
+		}
+	});
+
+	/* -------------------------------------
+			THEME ACCORDION
+	-------------------------------------- */
+	function themeAccordion() {
+		jQuery('.tg-panelcontent').hide();
+		jQuery('.tg-accordion h4:first').addClass('active').next().slideDown('slow');
+		jQuery('.tg-accordion h4').on('click',function() {
+			if(jQuery(this).next().is(':hidden')) {
+				jQuery('.tg-accordion h4').removeClass('active').next().slideUp('slow');
+				jQuery(this).toggleClass('active').next().slideDown('slow');
+			}
+		});
+	}
+	themeAccordion();
+});
