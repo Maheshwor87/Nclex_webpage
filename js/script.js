5c5c344a734fb9a9df12ef62c6caf27c02f8787f
nprdservice1 <nprdservice1@gmail.com>
2024-02-14 14:41:33 +0545

    fix: fix responsiveness of youtube iframes and add image gallery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.youtube-swiper', {
 
   slidesPerView: 1,
   spaceBetween: 20,
@@ -7,8 +7,10 @@ const swiper = new Swiper('.swiper', {
 
   allowSlidePrev: true,
   allowSlideNext: true,
-
+  
   loop: true,
+
+  speed: 1000,
   
   autoplay: {
     delay: 3000,
@@ -30,7 +32,6 @@ const swiper = new Swiper('.swiper', {
     }
   },
 
-  speed: 1000,
 
   navigation: {
     nextEl: '.swiper-button-next',
@@ -40,4 +41,50 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+});
+
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  direction: 'horizontal',
+
+  allowSlidePrev: true,
+  allowSlideNext: true,
+
+  loop: true,
+
+  speed: 5000,
+  
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: false,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,    
+      allowSlidePrev: true,
+      allowSlideNext: true,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      allowSlidePrev: true,
+      allowSlideNext: true,
+    }
+  },
+
+
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
