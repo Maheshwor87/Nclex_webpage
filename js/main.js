62b02789a26c27a03808173cd09be64683632b3a
nprdservice1 <nprdservice1@gmail.com>
2024-02-21 12:09:37 +0545

    fix: fix countdown end date

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ jQuery(document).on('ready', function() {
 			NEXT EVENT COUNTER
 	-------------------------------------- */
 	var _tg_upcomingeventcounter = jQuery('#tg-upcomingeventcounter');
-	_tg_upcomingeventcounter.countdown('2024/03/17', function(event) {
+	_tg_upcomingeventcounter.countdown('2024/03/09', function(event) {
 		var $this = jQuery(this).html(event.strftime(''
 			+ '<div class="tg-eventcounter"><span>%-D</span><span> Days Left</span></div>'
 			+ '<div class="tg-eventcounter"><span>%H</span><span>Hours</span></div>'
@@ -59,15 +59,15 @@ jQuery(document).on('ready', function() {
 	/* -------------------------------------
 			NEXT EVENT COUNTER
 	-------------------------------------- */
-	var _tg_upcomingeventcountervtwo = jQuery('#tg-upcomingeventcountervtwo');
-	_tg_upcomingeventcountervtwo.countdown('2024/02/07', function(event) {
-		var $this = jQuery(this).html(event.strftime(''
-			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%-D</span><span> Days</span></div></div>'
-			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%H</span><span>Hours</span></div></div>'
-			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%M</span><span>Minutes</span></div></div>'
-			+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%S</span><span>Seconds</span></div></div>'
-		));
-	});
+	// var _tg_upcomingeventcountervtwo = jQuery('#tg-upcomingeventcountervtwo');
+	// _tg_upcomingeventcountervtwo.countdown('2024/02/07', function(event) {
+	// 	var $this = jQuery(this).html(event.strftime(''
+	// 		+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%-D</span><span> Days</span></div></div>'
+	// 		+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%H</span><span>Hours</span></div></div>'
+	// 		+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%M</span><span>Minutes</span></div></div>'
+	// 		+ '<div class="tg-eventcounterholder"><div class="tg-eventcounter"><span>%S</span><span>Seconds</span></div></div>'
+	// 	));
+	// });
 	/* -------------------------------------
 			STICKY HEADER
 	-------------------------------------- */
