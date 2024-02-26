b15628abe89afa775e5b37fb9ee5ba07ffb42167
nprdservice1 <nprdservice1@gmail.com>
2024-02-26 15:28:28 +0545

    fix: add recent success heads

--- script.js
+++ script.js
@@ -48,3 +48,59 @@ expandableAbout.addEventListener('animationend', () => {
     expandableAbout.style.display = 'none';
   }
 });
+
+// Swiper options
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  direction: 'horizontal',
+
+  allowSlidePrev: true,
+  allowSlideNext: true,
+
+  loop: true,
+
+  speed: 5000,
+
+  autoplay: {
+    delay: 0,
+    pauseOnMouseEnter: false,
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      allowSlidePrev: true,
+      allowSlideNext: true,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      allowSlidePrev: true,
+      allowSlideNext: true,
+    },
+  },
+});
+
+const success = document.querySelector('.success-container');
+const viewAllBtn = document.getElementById('view-all');
+
+let timeoutId;
+
+viewAllBtn.addEventListener('click', function (e) {
+  if (success.style.maxHeight === '2000px') {
+    success.style.maxHeight = '400px';
+    success.style.transition = 'all 500ms ease';
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      viewAllBtn.innerText = 'View All';
+    }, 500);
+    return;
+  }
+  success.style.maxHeight = '2000px';
+  success.style.transition = 'all 500ms ease';
+  viewAllBtn.innerText = 'Collapse';
+});
